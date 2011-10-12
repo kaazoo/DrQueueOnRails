e95dd869ac4773c436df22e76b521f4bb9d3a427
Andreas Schroeder <andreas@drqueue.org>
2011-10-12 22:57:41 +0200

    add more keys specify collection name

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,15 @@
 class Job
   include MongoMapper::Document
 
-  key :name
+  set_collection_name "drqueue_jobs"
+
+  key :name, String
+  key :startframe, Integer
+  key :endframe, Integer
+  key :blocksize, Integer
+  key :renderer, String
+  key :scenefile, String
+  key :retries, Integer
+  key :owner, String
+
 end
