155de0f5f14cae079d702e12b2ad7c32fdd4d7cc
Andreas Schroeder <andreas@drqueue.org>
2011-11-27 21:41:57 +0100

    add utility functions for development (admin on/off) add actions for additional pages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,101 @@
 class MainController < ApplicationController
   before_filter :authenticate_user!
 
+
+  # start page
   def index
+
+    # force user to accept disclaimer
+    #if session[:profile].accepted == 0
+    #  redirect_to :action => 'disclaimer' and return
+    #end
+
+    # check for contacts file in public directory
+    contacts_file = File.join(Rails.root, '/public/contacts.html')
+    if File.exist?(contacts_file)
+      cf = File.open(contacts_file, "r")
+      @contacts = cf.read
+    else
+      @contacts = ""
+    end
+
   end
 
+
+  # TODO: remove later
+  def make_noadmin
+    current_user.update_attribute :admin, false
+    redirect_to :action => 'index' and return
+  end
+
+
+  # TODO: remove later
+  def make_admin
+    current_user.update_attribute :admin, true
+    redirect_to :action => 'index' and return
+  end
+
+
+  # tutorials page
+  def tutorials
+  end
+
+
+  # list of computers
+  def computers
+
+    cache_time = 60
+
+    if params[:id] != nil
+      # get info about special computer
+      puts @computer_info = $pyDrQueueClient.identify_computer(params[:id].to_i, cache_time)
+    else
+      # update list of all computers
+      @computer_list = []
+      $pyDrQueueClient.ip_client.ids.rubify.each do |comp_id|
+        puts info = $pyDrQueueClient.identify_computer(comp_id, cache_time)
+        @computer_list << info
+      end
+    end
+
+    # refresh timer
+    #  link = url_for(:controller => 'main', :action => 'computers', :id => params[:id], :protocol => ENV['WEB_PROTO']+"://")
+    #  if params[:refresh] != nil
+    #    if params[:refresh] == ""
+    #      @refresh_content = nil
+    #      session[:last_refresh] = nil
+    #    else
+    #      @refresh_content = params[:refresh]+'; URL='+link
+    #      session[:last_refresh] = params[:refresh]
+    #    end
+    #  elsif session[:last_refresh] != nil
+    #    @refresh_content = session[:last_refresh]+'; URL='+link
+    #  else
+    #    @refresh_content = '300; URL='+link
+    #  end
+
+  end
+
+
+  # cloudcontrol page
+  def cloudcontrol
+    # only admins are allowed to use cloudcontrol
+    if current_user.admin == false
+      redirect_to :controller => 'main' and return
+    else
+      # fetch all running sessions
+      @rendersessions = Rendersession.find(:all)
+
+      # fetch all unconnected payments
+      all_payments = Payment.find(:all)
+      @payments = []
+      all_payments.each do |pm|
+        puts pm.id
+        if Rendersession.find_by_payment_id(pm.id) == nil
+          @payments << pm
+        end
+      end
+    end
+  end
 
 end
