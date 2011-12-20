397e08c5caa596b88318a52fe73e697dfc3eda2f
Andreas Schroeder <andreas@drqueue.org>
2011-12-20 12:38:52 +0100

    add javascript function for costs calculation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,37 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$('form[data-remote]')
+$('a[data-remote],input[data-remote]')
+
+$('.mybutton').bind('ajax:beforeSend', function(){
+  alert("beforeSend!");
+  return false;
+});
+
+$('.mybutton').bind('ajax:success', function(){
+  alert("success!");
+  return false;
+});
+
+$(function () {
+  $('.mybutton').bind('ajax:beforeSend', function (evt, xhr, settings) {
+    alert("beforeSend!");
+    return false;
+  })
+  $('.mybutton').click(function () {
+    nums = document.getElementById("rendersession_num_slaves").value
+    rtime = document.getElementById("rendersession_run_time").value
+    vtype = document.getElementById("rendersession_vm_type").value
+    cdiv = document.getElementById("costs")
+
+    jQuery.post("/rendersessions/calculate_costs", { num_slaves: nums, run_time: rtime, vm_type: vtype }, function(data) {
+      $('#costs').html(data);
+    });
+
+    return false;
+    })
+});
+
