f90756a45277d657d73f4ad7523b637ea254debd
Andreas Schroeder <andreas@drqueue.org>
2012-01-30 22:23:25 +0100

    remove reference to payment model store PayPal token, Payer ID, pay timestamp

--- app/models/rendersession.rb
+++ app/models/rendersession.rb
@@ -2,20 +2,23 @@ class Rendersession
   include Mongoid::Document
   store_in "cloudcontrol_rendersessions"
 
-  belongs_to :payment
-  
   field :user, :type => String
   field :num_slaves, :type => Integer
   field :run_time, :type => Integer
+  field :vm_type, :type => String, :default => 't1.micro'
   field :costs, :type => Float
-#  field :payment_id, :type => Integer
+
+  field :paypal_token, :type => String
+  field :paypal_payer_id, :type => String
+  field :paid_at, :type => DateTime
+
   field :time_passed, :type => Integer, :default => 0
   field :start_timestamp, :type => Integer, :default => 0
   field :stop_timestamp, :type => Integer, :default => 0
   field :overall_time_passed, :type => Integer, :default => 0
-  field :vm_type, :type => String, :default => 't1.micro'
   
 #  attr_accessible :user, :num_slaves, :run_time, :time_passed
+
 
 end
 
